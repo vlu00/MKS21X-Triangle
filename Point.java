b795e13f826f09a83d9dfa2d2e2e5f22ad677ad7
vlu00 <vlu00@stuy.edu>
2018-10-11 16:47:01 -0400

    Fixed mistakes in Triangle and Point

--- Point.java
+++ Point.java
@@ -23,7 +23,7 @@ public class Point{
     return Math.sqrt(Math.pow(this.x - P2.x, 2) + Math.pow(this.y - P2.y, 2));
   }
 
-  public static double distanceBetween(Point P1, Point P2) {
+  public static double distance(Point P1, Point P2) {
     return Math.sqrt(Math.pow(P1.x - P2.x, 2) +
                      Math.pow(P1.x - P2.y, 2));
   }
--- Triangle.java
+++ Triangle.java
@@ -4,54 +4,53 @@ public class Triangle {
   private Point v3;
 
   public Triangle(Point P1, Point P2, Point P3) {
-    Point a = new Point (P1);
-    Point b = new Point (P2);
-    Point c = new Point (P3);
-    v1 = a;
-    v2 = b;
-    v3 = c;
+	Point a = new Point (P1);
+	Point b = new Point (P2);
+	Point c = new Point (P3);
+	v1 = a;
+	v2 = b;
+	v3 = c;
   }
 
   public Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
-    Point P1 = new Point (x1, y1);
-    Point P2 = new Point (x2, y2);
-    Point P3 = new Point (x3, y3);
-    v1 = P1;
-    v2 = P2;
-    v3 = P3;
+	Point P1 = new Point (x1, y1);
+	Point P2 = new Point (x2, y2);
+	Point P3 = new Point (x3, y3);
+	v1 = P1;
+	v2 = P2;
+	v3 = P3;
   }
 
   public double getPerimeter() {
-    return v1.distanceTo(v2) +
-           v2.distanceTo(v3) +
-           v3.distanceTo(v1);
+	return v1.distanceTo(v2) +
+       	v2.distanceTo(v3) +
+       	v3.distanceTo(v1);
   }
 
   public Point getVertex(int index) {
-    if (index == 0) {
-      return v1;
-    }
-    if (index == 1) {
-      return v2;
-    }
-    else return v3;
+	if (index == 0) {
+  	return new Point(v1);
+	}
+	if (index == 1) {
+  	return new Point (v2);
+	}
+	else
+  	return new Point (v3);
   }
 
   public void setVertex(int index, Point newP) {
-    if (index == 0) {
-      v1 = newP;
-    }
-    if (index == 1) {
-      v2 = newP;
-    }
-    else
-      v3 = newP;
+	if (index == 0) {
+  	v1 = new Point (newP);
+	}
+	if (index == 1) {
+  	v2 = new Point (newP);
+	}
+	else
+  	v3 = new Point (newP);
   }
+
 
   public String toString() {
-    return "Triangle: " + "A" + "(" + v1.getX() + ", " + v1.getY() + ")" + " " +
-                          "B" + "(" + v2.getX() + ", " + v2.getY() + ")" + " " +
-                          "C" + "(" + v3.getX() + ", " + v3.getY() + ")";
+	return "Triangle: " + "A" + v1 + "B" + v2 + "C" + v3;
   }
-
 }
